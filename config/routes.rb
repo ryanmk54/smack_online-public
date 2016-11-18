b4c92d6203969ee4e449a05998b69698f04c0e74
Ryan Kingston <ryanmk54@gmail.com>
2016-11-18 20:31:51 +0000

    Removed index and destroy from projects routes. Added receive_service_output.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  resources(:projects, except: [:index, :destroy]) do
+    member do
+      post 'receive_service_output'
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
