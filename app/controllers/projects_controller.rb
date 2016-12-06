c288e493e2991e1253154755cd672613bccd301f
Matthew Mendez <matthewmendez@gmail.com>
2016-12-06 11:52:39 -0700

    Added back in protect from forgery :null_session lines as a temp fix for the SMACK server to be able to contact the web server

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,5 @@
 class ProjectsController < ApplicationController
+  protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format == 'application/json'}
   before_action :set_project, only: [:show, :edit, :update]
 
   # Production SMACK server URL
