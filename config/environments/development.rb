2c77f60ad35182ff9a6e7f5ea226cab6a0af1fbe
Ryan <ryanmk54@gmail.com>
2016-12-09 11:54:10 -0700

    make assets more readable in development mode

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,6 +45,9 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  # RK added to make it more readable in Firefox
+  config.assets.digest = false
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
