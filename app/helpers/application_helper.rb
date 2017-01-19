7e37b8eac6423f5d41b980c117a6e0969c6ab2f8
Ryan Kingston <ryanmk54@gmail.com>
2017-01-19 16:46:28 -0700

    Flash messages are now dismissable

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+def flash_class(level)
+  case level
+    when 'notice'  then "flash alert alert-dismissable alert-info"
+    when 'success' then "flash alert alert-dismissable alert-success"
+    when 'error'   then "flash alert alert-dismissable alert-error"
+    when 'alert'   then "flash alert alert-dismissable alert-error"
+  end
+end
+
 end
