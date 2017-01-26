6dc893887c668db7e8f6e7831f513641fb618ab0
Ryan Kingston <ryanmk54@gmail.com>
2017-01-26 15:40:53 -0700

    Fix email bug with removal accessor from User

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,6 @@ class User < ApplicationRecord
   # has_many :projects, through: :project_users
   has_and_belongs_to_many :projects
 
-  attr_accessor :id, :email, :name
-
   def private_projects
     self.projects
   end
@@ -23,13 +21,6 @@ class User < ApplicationRecord
   # def currently_running_projects
   #   self.projects.
   # end
-
-
-
-
-
-
-
 end
 
 
