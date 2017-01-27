df0c3cafdceb128995fd05fa5abc08e8c2102b97
Ryan Kingston <ryanmk54@gmail.com>
2017-01-27 16:07:33 -0700

    Append to .bashrc instead of overwriting
    The installer sources chruby by adding to .bashrc. Previously it
    used > to overwrite it. My mistake. It should use >> to append.

--- bin/install.sh
+++ bin/install.sh
@@ -4,16 +4,13 @@
 command -v sudo >/dev/null 2>&1 || { echo >&2 "sudo is required"; exit 1; }
 command -v apt-get >/dev/null 2>&1 || { echo >&2 "apt-get is required"; exit 1; }
 command -v wget >/dev/null 2>&1 || { echo >&2 "wget is required"; exit 1; }
+command -v make >/dev/null 2>&1 || { sudo apt-get install make; };
+  # make is a prereq for ruby-install and chruby
 
-if !(command -v ruby-install >/dev/null 2>&1 &&
-     command -v chruby >/dev/null 2>&1)
+if !(command -v ruby-install >/dev/null 2>&1)
 then
-  command -v make >/dev/null 2>&1 || { sudo apt-get install make; };
-    # make is a prereq for ruby-install and chruby
-
   # Change to a temporary directory so stuff isn't cluttered
   cd /tmp
-
   # Install ruby-install
   wget -O ruby-install-0.6.0.tar.gz https://github.com/postmodern/ruby-install/archive/v0.6.0.tar.gz;
   tar -xzvf ruby-install-0.6.0.tar.gz;
@@ -22,6 +19,12 @@ then
 
   # Install ruby 2.3.1
   ruby-install ruby-2.3.1;
+fi
+
+if !( command -v chruby >/dev/null 2>&1)
+then
+  # Change to a temporary directory so stuff isn't cluttered
+  cd /tmp
 
   # Install chruby
   wget -O chruby-0.3.9.tar.gz https://github.com/postmodern/chruby/archive/v0.3.9.tar.gz;
@@ -29,9 +32,9 @@ then
   cd chruby-0.3.9/;
   sudo make install;
 
-  echo "source /usr/local/share/chruby/chruby.sh" > ~/.bashrc;
+  echo "source /usr/local/share/chruby/chruby.sh" >> ~/.bashrc;
     # add the chruby command to the path
-  echo "source /usr/local/share/chruby/auto.sh" > ~/.bashrc;
+  echo "source /usr/local/share/chruby/auto.sh" >> ~/.bashrc;
     # adds auto-switching to chruby
 fi
 
