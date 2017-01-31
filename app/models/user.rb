f2ac84d2ea66aa0e9f262b3f01e44cf26c763370
christobowcutt <christopher.bowcutt@utah.edu>
2017-01-30 17:06:13 -0700

    Users can now upload photos on the edit page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,13 @@
 class User < ApplicationRecord
-
   # Include default devise modules. Others available are:
   # :omniauthable, :lockable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable, :timeoutable
+
+
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
   # has_many :project_users
   # has_many :projects, through: :project_users
--- config/routes.rb
+++ config/routes.rb
@@ -16,5 +16,6 @@ Rails.application.routes.draw do
       post 'receive_service_output', to: 'projects#receive_service_output'
     end
   end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
