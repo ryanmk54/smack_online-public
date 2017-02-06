7f6a6e4b1e2c63a74c4f194eb576458af7dc4a1e
Christopher Bowcutt <bowcuttchris@gmail.com>
2017-02-06 17:43:09 +0000

    adding usernames to profile, signup, and user registrations page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,9 @@ class ApplicationController < ActionController::Base
 
 
   def configure_permitted_parameters
-    added_attrs = [:email, :password, :current_password, :password_confirmation, :avatar]
-    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+    update_attrs = [:email, :password, :current_password, :password_confirmation, :avatar]
+    sign_up_attrs = [:email, :password, :username, :password_confirmation, :avatar]
+    devise_parameter_sanitizer.permit :account_update, keys: update_attrs
+    devise_parameter_sanitizer.sign_up :account_update, keys: sign_up_attrs
   end
 end
