deb26974b6f6b7110e0dca76197c872bf155d68a
christobowcutt <christopher.bowcutt@utah.edu>
2017-02-06 11:04:01 -0700

    adding usernames to view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,9 @@ class ApplicationController < ActionController::Base
 
 
   def configure_permitted_parameters
-    update_attrs = [:email, :password, :current_password, :password_confirmation, :avatar]
+    update_attrs = [:email, :password, :current_password, :password_confirmation, :avatar, :username]
     sign_up_attrs = [:email, :password, :username, :password_confirmation, :avatar]
     devise_parameter_sanitizer.permit :account_update, keys: update_attrs
-    devise_parameter_sanitizer.sign_up :account_update, keys: sign_up_attrs
+    devise_parameter_sanitizer.permit :sign_up, keys: sign_up_attrs
   end
 end
